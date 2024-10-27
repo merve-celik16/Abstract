8c953af832dfb607a091e4a6e5ba5597addda7e0
merve-celik16 <mervedursunn34@gmail.com>
2024-10-28 00:54:00 +0300

    Proje dosyası ekle.

---
+++ Calisan.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public abstract class Calisan
+    {
+        public string Ad{ get; set; }
+        public string Soyadi { get; set; }
+        public string Departman { get; set; }
+        public abstract void Gorev();
+       
+        
+    }
+    public class MagazaYoneticisi :Calisan 
+    {
+       
+
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyadi} , Mağaza yöneticisi olarak görev yapmaktayım..");
+        }
+    }
+    public class ProjeYoneticisi : Calisan
+    {
+        public override void Gorev()
+
+        {
+            Console.WriteLine($"{Ad} {Soyadi} , Proje yöneticisi olarak görev yapmaktayım..");
+        }
+    }
+
+    public class SatisTemsilcisi : Calisan
+    {
+        public override void Gorev()
+
+        {
+            Console.WriteLine($"{Ad} {Soyadi} , Satış temsilcisi olarak görev yapmaktayım..");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Abstract
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           Calisan magazaYoneticisi = new MagazaYoneticisi { Ad = "Mehmet", Soyadi = "Yüksel", Departman = "Mağaza Müdürü" };
+            Calisan projeYoneticisi = new ProjeYoneticisi { Ad = "Merve", Soyadi = "Çelik", Departman = "Proje Yönetimi" };
+            SatisTemsilcisi satisTemsilcisi = new SatisTemsilcisi { Ad = "Enes", Soyadi = "Kaya", Departman = "Satış" };
+
+            magazaYoneticisi.Gorev();
+            projeYoneticisi.Gorev();
+            satisTemsilcisi.Gorev();
+        }
+    }
+}
